6664c0c1c7398feb0cf617d43a7a75226133ea3b
MichalFrater <miskofrater@gmail.com>
2023-09-03 19:50:50 +0200

    Added ImGui Events, Including input

--- Coffee/src/Coffee/Application.cpp
+++ Coffee/src/Coffee/Application.cpp
@@ -1,20 +1,31 @@
 #include "cfpch.h"
 #include "Application.h"
 
-#include "Coffee/Events/ApplicationEvent.h"
 #include "Coffee/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Coffee {
 
+#define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
+
+
 	Application::Application() {
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 	}
 
 	Application::~Application() {
 
 	}
+	
+	void Application::OnEvent(Event& e) {
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(Application::OnWindowClose));
+
+		LOG_CORE_TRACE("{0}", e);
+	}
+
 
 	void Application::Run() {		
 		while (m_Running) {
@@ -24,6 +35,10 @@ namespace Coffee {
 		}
 	}
 
+	bool Application::OnWindowClose(WindowCloseEvent& e) {
+		m_Running = false;
+		return true;
+	}
 
 }
 
